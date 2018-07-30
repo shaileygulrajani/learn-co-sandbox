eb68702302628e89b5779568756f44069dced6f1
shaileygulrajani <shaileygulrajani@gmail.com>
2018-07-30 14:20:06 +0000

    automatically backed up by learn

---
+++ mafia.rb
@@ -0,0 +1,7 @@
+puts "Mafia, who would you like to kill?"
+victim = gets.chomp
+puts "Detective, who would you like to investigate?"
+suspect = gets.chomp 
+puts "Doctor, who would you like to save?"
+patient = gets.chomp 
+puts "In Killadelphia, the mafia killed #{victim}, detective thought it was #{suspect} while the doctor saved #{patient}"
---
+++ selfcode.rb
@@ -0,0 +1,9 @@
+puts "What is your name?"
+name = gets.chomp 
+puts "How old are you?"
+age = gets.chomp 
+puts "What is your favorite food?"
+food = gets.chomp 
+puts "What is your zodiac sign?"
+zodiac = gets.chomp
+puts "The user's name is #{name}, is #{age} years old, loves #{food}, and is a #{zodiac}"
